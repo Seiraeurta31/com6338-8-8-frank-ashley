087767ff2d4b650421d3918e138867e6dbf67380
Ashley Frank <ashley.frank@ufl.edu>
2023-07-13 20:06:48 -0400

    added fetch code for city

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+//API key 08ace7633004d5ddf370678a8c052e90
+
+var weatherDisplay = document.getElementById('weather')
+var form = document.querySelector('form')
+
+form.onsubmit = function(e) {
+    e.preventDefault()
+    //console.log("hello")
+    var userInput = document.querySelector('input').value.trim()
+    //console.log (userInput)
+
+    if(!userInput) return
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=08ace7633004d5ddf370678a8c052e90')
+    .then(function(res){
+        //console.log (res)
+        return res.json()
+    })
+    .then(function(data){
+        console.log (data)
+    })
+
+}
