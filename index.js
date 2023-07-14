0952380d492bdf256d7632451edc90441389ec9d
Ashley Frank <ashley.frank@ufl.edu>
2023-07-13 20:48:49 -0400

    tweaks for form.onsubmit

--- index.js
+++ index.js
@@ -7,18 +7,30 @@ var form = document.querySelector('form')
 form.onsubmit = function(e) {
     e.preventDefault()
     //console.log("hello")
-    var userInput = document.querySelector('input').value.trim()
-    //console.log (userInput)
+    var userInput = this.search.value.trim()
+    console.log (userInput)
 
     if(!userInput) return
 
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=08ace7633004d5ddf370678a8c052e90')
     .then(function(res){
         //console.log (res)
+        if(res.status !== 200){
+            throw new Error ('Location not found')
+        }
         return res.json()
+        console.log(res)
     })
-    .then(function(data){
-        console.log (data)
-    })
+    .then(formatLocationData)
+    .catch(function(err){
+        weatherDisplay.innerHTML = err.message
+    })   
 
+}
+
+function formatLocationData(locationData){
+    weatherDisplay.innerHTML = ""
+    userInput.value =""
+    var cityName = document.createElement('h3')
+    //cityName.textContent
 }
